d029bab50fe0e4d596b14cd0241b2678bc9b6b5c
Laven Sun <38180984+LavenSun@users.noreply.github.com>
2020-02-09 17:07:07 +0800

    Add files via upload

---
+++ 4.21.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+struct inflatable
+{
+  char name[20];
+  float volume;
+  double price;
+};
+int main() //使用new创建动态结构
+{
+  inflatable * ps = new inflatable;
+  cout << "Enter name of inflatable item: ";
+  cin.get(ps -> name, 20);
+  cout << "Enter volume in cubic feet: ";
+  cin >> (*ps).volume;
+  cout << "Enter price: $";
+  cin >> ps -> price;
+  cout << "Name: " << (*ps).name << endl;
+  cout << "Volume: " << ps -> volume << " cubic feet\n";
+  cout << "Price: " << ps -> price << endl;
+  delete ps;
+  return 0;
+}
